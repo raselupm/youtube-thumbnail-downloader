2e34e388110d77722059e1d7c686b84f1371a7b4
raselupm <wp@perfectpointmarketing.com>
2024-03-05 10:16:48 +0300

    Download links

--- app.js
+++ app.js
@@ -12,12 +12,24 @@ app.get('/thumbnail', async (req, res) => {
 
     try {
         const info = await ytdl.getInfo(videoUrl);
+        let video = '';
+        let audio = '';
+        info.formats.forEach((format) => {
+            if(format.mimeType.includes('video/mp4') && format.qualityLabel === '720p'){
+                video = format.url;
+            }
+
+            if(format.mimeType.includes('audio/mp4')){
+                audio = format.url;
+            }
+        });
+        //console.log(info.formats);
         const thumbnails = info.videoDetails.thumbnails;
         thumbnails.reverse();
         // only take first two items in the array
-        thumbnails.length = 2;
+        thumbnails.length = 1;
         const thumbnailUrl = thumbnails[0].url;
-        res.json({ thumbnailUrl });
+        res.json({ thumbnailUrl, video, audio});
     } catch (error) {
         console.error('Error:', error);
         res.status(500).json({ error: 'Unable to fetch thumbnail' });
